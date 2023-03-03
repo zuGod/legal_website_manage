ad4140bdc54d82575ab3fbade105a7a96bb428fc
zuzu <2497171328@qq.com>
2023-03-03 15:04:00 +0800

    faxin

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="publish-container">
-    发布文章
+    <el-card class="box-card">
+      <div slot="header"
+           class="clearfix">
+        <!-- 面包屑 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right"
+                       class="breadcrumb">
+          <el-breadcrumb-item :to="{ path: '/' }"
+                              class="first">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>发布文章</el-breadcrumb-item>
+        </el-breadcrumb>
+        <span>卡片名称</span>
+        <el-button style="float: right; padding: 3px 0"
+                   type="text">操作按钮</el-button>
+      </div>
+      <div v-for="o in 4"
+           :key="o"
+           class="text item">
+        {{'列表内容 ' + o }}
+      </div>
+    </el-card>
   </div>
 </template>
 
